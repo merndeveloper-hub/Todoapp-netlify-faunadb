81aca743808562d68b6b80fd5cf1205f3f9690ab
Dreamcrushers-sj <wangchaoiq@gmail.com>
2021-05-05 10:02:54 -0700

    changing

---
+++ src/components/AppHeader/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import deployButton from '../../assets/deploy-to-netlify.svg'
+import logo from '../../assets/logo.svg'
+import styles from './AppHeader.css' // eslint-disable-line
+
+const AppHeader = (props) => {
+  return (
+    <header className='app-header'>
+      <div className='app-title-wrapper'>
+        <div className='app-title-wrapper'>
+          <div className='app-left-nav'>
+            <img src={logo} className='app-logo' alt='logo' />
+            <div className='app-title-text'>
+              <h1 className='app-title'>Netlify + Fauna DB</h1>
+              <p className='app-intro'>
+                Using FaunaDB & Netlify functions
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='deploy-button-wrapper'>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://app.netlify.com/start/deploy?repository=https://github.com/netlify/netlify-faunadb-example&stack=fauna'>
+            <img src={deployButton} className='deploy-button' alt='deploy to netlify' />
+          </a>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default AppHeader
